09d26a787409e2066e99395c5ee6876ad7c470da
Paris Karagiannopoulos <parisk85@gmail.com>
2020-08-27 22:53:20 +0300

    Tests first commit

---
+++ src/test/groovy/gr/parisk85/jare/core/BasicRuleTest.groovy
@@ -0,0 +1,54 @@
+package gr.parisk85.jare.core
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class BasicRuleTest extends Specification {
+
+    @Unroll
+    def "expects to run then clause only when condition is true"() {
+        given:
+        def actual = new StringBuilder("This is");
+
+        def tester = RuleBuilder.given(StringBuilder.class)
+                .when { it -> condition }
+                .then { it -> it << " a test." }
+
+        when:
+        tester.run(actual)
+
+        then:
+        expected == actual.toString()
+
+        where:
+        condition | expected
+        true      | "This is a test."
+        false     | "This is"
+    }
+
+    def "expect to throw when condition is true"() {
+        given:
+        def tester = RuleBuilder.given(Boolean.class)
+                .when { it }
+                .thenThrow { RuntimeException }
+
+        when:
+        tester.run(true)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "expect not to throw when condition is false"() {
+        given:
+        def tester = RuleBuilder.given(Boolean.class)
+                .when { it }
+                .thenThrow { RuntimeException }
+
+        when:
+        tester.run(false)
+
+        then:
+        noExceptionThrown()
+    }
+}
---
+++ src/test/groovy/gr/parisk85/jare/core/JAREngineTest.groovy
@@ -0,0 +1,6 @@
+package gr.parisk85.jare.core
+
+import spock.lang.Specification
+
+class JAREngineTest extends Specification {
+}
